4ccf736dc001f28e6b94ab8091e7dc3d585fd7d2
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-08-16 12:38:40 -0400

    had some issues with the last three but i got it

--- test/indexTest.js
+++ test/indexTest.js
@@ -41,6 +41,7 @@ describe('index.js', function () {
   });
 
   describe('driverNamesWithRevenueOver()', function () {
+    console.log(driverNamesWithRevenueOver(drivers, 199))
     it('returns an array of all matching drivers names as string', function () {
       expect(driverNamesWithRevenueOver(drivers, 199)).to.eql([ 'Sally', 'Annette' ]);
     });
